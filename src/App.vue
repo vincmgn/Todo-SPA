a3b6f4a1ef56572808ceafa812ff106d3b35999e
vincmgn <vincentmagnienpro@gmail.com>
2024-06-10 21:20:20 +0200

    feat: header & footer + switch theme

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
-
-    <AppFooter />
-  </v-app>
+  <VApp>
+    <Header />
+    <VMain>
+      <!--TODO-->
+    </VMain>
+    <Footer />
+  </VApp>
 </template>
 
 <script setup>
-  //
+import { VMain, VApp } from "vuetify/components";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 </script>
+
+<style scoped></style>
