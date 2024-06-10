b2256063147988f37d14f0b28a90f3b972894584
vincmgn <vincentmagnienpro@gmail.com>
2024-06-11 01:50:04 +0200

    feat: UI + form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,40 @@
 <template>
-  <VAppBar>
-    <VAppBarTitle>🎯 Todo-SPA | Viviarto</VAppBarTitle>
+  <VAppBar class="px-5">
+    <!-- Title -->
+    <VAppBarTitle>🎯 Todo-SPA</VAppBarTitle>
+    <!-- Spacer -->
     <VSpacer />
+    <!-- Dark theme Switch -->
     <VSwitch
       hide-details
       class="mr-4"
       v-model="darkTheme"
-      :label="darkTheme ? 'Light Theme' : 'Dark Theme'"
+      :label="darkTheme ? 'Dark Theme' : 'Light Theme'"
       @change="toggleTheme"
     />
   </VAppBar>
 </template>
 
 <script setup>
+// IMPORTS
 import { useTheme } from "vuetify";
 import { VAppBar, VAppBarTitle, VSpacer, VSwitch } from "vuetify/components";
 
+// DATA
 const theme = useTheme();
+const darkTheme = theme.global.current.value.dark; // setup dark theme
 
+// METHODS
 function toggleTheme() {
+  // if dark theme is enabled, switch to light theme
   theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+@media screen and (max-width: 650px) {
+  .v-spacer {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- Container TODO -->
+  <VContainer fluid class="d-flex align-center justify-center fill-height">
+    <VRow class="d-flex justify-center align-center">
+      <VCol cols="12" sm="10" md="8" lg="4" xl="2">
+        <!-- Title -->
+        <h1 class="d-flex justify-center pb-5">CREATE A TODO</h1>
+        <!-- Form -->
+        <VForm @submit.prevent="addTodo">
+          <VTextField
+            v-model="input_title"
+            label="Title"
+            variant="outlined"
+            class="mb-4"
+          />
+          <VTextarea
+            v-model="input_description"
+            label="Description"
+            clear-icon="mdi-close-circle"
+            variant="outlined"
+            clearable
+            class="mb-4"
+          />
+          <VBtn
+            prepend-icon="mdi-plus"
+            variant="flat"
+            color="teal-darken-2"
+            type="submit"
+            class="w-100"
+          >
+            Add a todo
+          </VBtn>
+        </VForm>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import {
+  VContainer,
+  VRow,
+  VCol,
+  VForm,
+  VTextField,
+  VTextarea,
+  VBtn,
+} from "vuetify/components";
+
+// DATA
+const input_title = ref("");
+const input_description = ref("");
+
+// METHODS
+const addTodo = () => {
+  // check if input fields are empty
+  if (
+    input_title.value.trim() === "" ||
+    input_description.value.trim() === ""
+  ) {
+    return;
+  }
+  console.log(input_title.value, input_description.value);
+};
+</script>
+
+<style scoped>
+.fill-height {
+  height: 100vh;
+}
+</style>
